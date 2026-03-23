d65bbf78666e31a90ccf916b4572309646b6fba4
E-ROAD <motor1688@gmail.com>
2026-03-23 11:00:01 +0800

    Update check-promo.js

--- scripts/check-promo.js
+++ scripts/check-promo.js
@@ -6,6 +6,10 @@ const STATUS_FILE = 'promo_status.json';
 function fetchPage(url) {
   return new Promise((resolve, reject) => {
     https.get(url, { headers: { 'User-Agent': 'Mozilla/5.0' } }, (res) => {
+      if (res.statusCode >= 300 && res.statusCode < 400 && res.headers.location) {
+        const redirectUrl = res.headers.location.startsWith('http') ? res.headers.location : new URL(res.headers.location, url).href;
+        return fetchPage(redirectUrl).then(resolve).catch(reject);
+      }
       let data = '';
       res.on('data', chunk => data += chunk);
       res.on('end', () => resolve(data));
@@ -123,15 +127,8 @@ async function checkPromo() {
   if (needReset) { autoloadFull = false; autoloadMsg = ''; }
   
   try {
-   const page12654raw = await fetchPage('https://www.icash.com.tw/Home/NewsDetail/?ID=12654');
+    const page12654raw = await fetchPage('https://www.icash.com.tw/Home/NewsDetail/?ID=12654');
     const page12654 = page12654raw.replace(/<[^>]+>/g, '');
-    // debug: 找頁面中有沒有額滿字樣
-    const idxFull = page12654.indexOf('額滿');
-    if (idxFull >= 0) {
-      console.log('[ID=12654] DEBUG 額滿附近: ' + page12654.substring(Math.max(0, idxFull - 80), idxFull + 20).replace(/\n/g, ' '));
-    } else {
-      console.log('[ID=12654] DEBUG 頁面中找不到「額滿」');
-    }
     const regexAutoload = new RegExp(monthNum + '月[\\s\\S]*?自動加值[\\s\\S]*?額滿');
     const matchAutoload = page12654.match(regexAutoload);
     if (matchAutoload) {
